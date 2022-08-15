8cd8cb76f9f332c1f06ed9dadce7a1389b8ff746
johtai <johtai@yandex.ru>
2022-08-15 17:09:09 +0300

    clear code, remove try-except

--- script.py
+++ script.py
@@ -3,25 +3,23 @@ from bs4 import BeautifulSoup
 
 
 url = sys.argv[1]
-print(url)
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 links = soup.find_all('a')
 
 for link in links:
     url = link.get('href')
-    #print(url)
     if url and "@" in url:
         url = "https://vk.com" + url[:url.index("?")]
+        
         print(url)
+        
         response = urllib.request.urlopen(url)
         webContent = response.read().decode('UTF-8')
 
-        try:
+        if os.path.isdir("topics"):
             os.mkdir("topics")
-        except FileExistsError:
-            pass
-
+            
         f = open(f"topics/{url[url.find('@'):]}.html", 'w', encoding="UTF-8")
         f.write(webContent)
         f.close
